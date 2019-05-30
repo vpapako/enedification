3a570c6f0d45054a47f89815af5916b8e2c7145e
Vassilis Papakonstantinou <vpapako@users.noreply.github.com>
2019-05-30 13:10:22 +0300

    Update build-with-docker.sh

--- datacenter-agent/build-with-docker.sh
+++ datacenter-agent/build-with-docker.sh
@@ -2,5 +2,5 @@
 
 
 #echo $PWD
-#docker run --rm -v "$GOPATH/src/":/go/src/ -w /go/src/enedification/datacenter-agent golang:1.11 go build -v
-docker run --rm -v "$GOPATH/src/":/go/src/ -w /go/src/github.com/enedification/datacenter-agent golang:1.11 go build -v
+docker run --rm -v "$GOPATH/src/":/go/src/ -w /go/src/enedification/datacenter-agent golang:1.11 go build -v
+#docker run --rm -v "$GOPATH/src/":/go/src/ -w /go/src/github.com/enedification/datacenter-agent golang:1.11 go build -v
